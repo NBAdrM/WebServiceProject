07c698fab0c67c32c4a4e16468c5f37f8a525d50
NBAdrM <91313874+NBAdrM@users.noreply.github.com>
2023-04-21 11:44:34 +0200

    Update password

--- project.shop.web/src/main/java/project/shop/web/data/ConnectBD.java
+++ project.shop.web/src/main/java/project/shop/web/data/ConnectBD.java
@@ -7,7 +7,7 @@ public class ConnectBD {
 	public Order putOrder(Order o) {
 		try {
 		Class.forName("com.mysql.jdbc.Driver");
-		Connection conn = DriverManager.getConnection("jdbc:mysql://mysql-maestri.alwaysdata.net:3306/maestri_web_service", "maestri" , "Suzuki1100!");
+		Connection conn = DriverManager.getConnection("jdbc:mysql://mysql-maestri.alwaysdata.net:3306/maestri_web_service", "maestri" , "*******");
 		Statement stmt = conn.createStatement();
 		String query = "INSERT INTO `command` (`id_client`, `id_produc`, `id_order`, `date`, `nb_product`) VALUES ('"+o.getId_client()+"', '"+o.getId_product()+"', '"+o.getId_order()+"', CURRENT_TIMESTAMP, '"+o.getNb_product()+"');";
 		stmt.executeUpdate(query);
